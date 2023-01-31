88228387ca21155785e939f68c3fe72b0c5955a6
TheLittle_Yang <2570984321@qq.com>
2023-01-31 18:36:15 +0800

    🌈 style: 首页修改。

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -133,7 +133,7 @@ const config = {
             ],
           },
         ],
-        copyright: `版权所有 © 2019-${new Date().getFullYear()} 厕所总部，使用 💙 和 Docusaurus 构建。 </br> <a href="https://beian.miit.gov.cn/" target=“_blank”>浙ICP备2021038660号-4</a>`,
+        copyright: `版权所有 © 2019-${new Date().getFullYear()} 厕所总部。 </br> <a href="https://beian.miit.gov.cn/" target=“_blank”>浙ICP备2021038660号-4</a>`,
       },
       prism: {
         theme: lightCodeTheme,
