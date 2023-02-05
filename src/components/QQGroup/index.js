83f03c9446e21a38d8cf8b72aa0363cd75d2b8d0
shikukuya <89395757+shikukuya@users.noreply.github.com>
2023-02-05 21:41:44 +0800

    ✨ feat: 添加<QQGroup />组件

---
+++ src/components/QQGroup/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function QQGroup() {
+  return <a href="http://jq.qq.com/?_wv=1027&k=Uc4Zt2F1" style={{ zoom: '120%' }}><img src="http://img.shields.io/badge/QQ群--点击加入-720737548-orange?style=social" /></a>;
+}
