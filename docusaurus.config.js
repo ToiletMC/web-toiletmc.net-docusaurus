e4ae0de045a09e0d80e0daaf703c145353cb806a
TheLittle_Yang <46647646+XXY233@users.noreply.github.com>
2023-03-02 12:18:29 +0800

    fix 404

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ToiletMC', // Usually your GitHub org/user name.
   projectName: 'toiletmc.net', // Usually your repo name.
-  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
