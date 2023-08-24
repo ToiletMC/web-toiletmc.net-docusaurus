f32ea591ff9973f2a243a6c791126e3720a7e894
TheLittle_Yang <2570984321@qq.com>
2023-08-24 21:39:38 +0800

    Update about.jsx

---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+export default function About() {
+  return (
+    <Layout title={`关于`} description="关于">
+      <main></main>
+    </Layout>
+  );
+}
